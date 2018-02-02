97752005060f1d22115757127c2162689155e689
mremington <michael.remington@sduhsd.net>
2018-02-01 18:24:57 -0800

    added a print line

--- APCSA/src/Unit0/Lab0a3.java
+++ APCSA/src/Unit0/Lab0a3.java
@@ -19,5 +19,6 @@ public class Lab0a3
 		System.out.println("\\\\ draws one backslash on the screen!\n" );
 		System.out.println("\\\" draws one double quote on the screen!\n" );
 		System.out.println("\\\' draws one single quote on the screen!\n" );
+		System.out.println("test git hub changes");
 	}
 }
